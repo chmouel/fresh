6d31967157220c17c4810721bf18ace8eae51936
Chmouel Boudjnah <chmouel@chmouel.com>
2016-11-28 08:21:12 +0100

    Allow passing args in config

--- runner/runner.go
+++ runner/runner.go
@@ -10,7 +10,7 @@ func run() bool {
 
 	runnerLog("Running...")
 
-	cmd := exec.Command(buildPath())
+	cmd := exec.Command(buildPath(), argName())
 
 	stderr, err := cmd.StderrPipe()
 	if err != nil {
